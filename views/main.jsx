863fea92ba4869b006ff4be8befafa184185f16b
StanPri <spribytkovsky@yahoo.com>
2017-01-26 15:52:23 -0800

    Todo added

--- views/main.jsx
+++ views/main.jsx
@@ -4,6 +4,10 @@ import React from 'react';
 
 export default class Main extends React.Component {
   render() {
-    return <div>Hello from React with ES6 :)</div>;
+    return
+      <div>
+        Hello from React with ES6 :)
+      </div>;
+      //TODO: add more components and test them with electron
   }
 }
